b3a7f9558f3a5722c6266e5afb588a278a1a78c9
Aayush Gupta <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2023-10-11 23:18:21 +0530

    Added Name import (#34)

--- test/mocks/LinkToken.sol
+++ test/mocks/LinkToken.sol
@@ -3,7 +3,7 @@
 // @dev This contract has been adapted to fit with dappTools
 pragma solidity ^0.8.0;
 
-import "@solmate/tokens/ERC20.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
 
 interface ERC677Receiver {
     function onTokenTransfer(
