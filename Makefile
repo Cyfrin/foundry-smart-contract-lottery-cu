24c3da32f90f8fec56e00a61415e68b968a6ec2b
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2024-06-02 13:06:17 -0400

    2024 update

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ clean  :; forge clean
 # Remove modules
 remove :; rm -rf .gitmodules && rm -rf .git/modules/* && rm -rf lib && touch .gitmodules && git add . && git commit -m "modules"
 
-install :; forge install Cyfrin/foundry-devops@0.2.0 --no-commit && forge install smartcontractkit/chainlink-brownie-contracts@1.1.1 --no-commit && forge install foundry-rs/forge-std@v1.8.3 --no-commit && forge install transmissions11/solmate@v6 --no-commit
+install :; forge install Cyfrin/foundry-devops@0.2.0 --no-commit && forge install smartcontractkit/chainlink-brownie-contracts@1.1.1 --no-commit && forge install foundry-rs/forge-std@v1.8.2 --no-commit && forge install transmissions11/solmate@v6 --no-commit
 
 # Update Dependencies
 update:; forge update
--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -10,51 +10,30 @@ contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
         AddConsumer addConsumer = new AddConsumer();
-        (
-            uint64 subscriptionId,
-            bytes32 gasLane,
-            uint256 automationUpdateInterval,
-            uint256 raffleEntranceFee,
-            uint32 callbackGasLimit,
-            address vrfCoordinatorV2,
-            address link,
-            uint256 deployerKey
-        ) = helperConfig.activeNetworkConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
-        if (subscriptionId == 0) {
+        if (config.subscriptionId == 0) {
             CreateSubscription createSubscription = new CreateSubscription();
-            (subscriptionId, vrfCoordinatorV2) = createSubscription.createSubscription(
-                vrfCoordinatorV2,
-                deployerKey
-            );
+            (config.subscriptionId, config.vrfCoordinatorV2_5) =
+                createSubscription.createSubscription(config.vrfCoordinatorV2_5);
 
             FundSubscription fundSubscription = new FundSubscription();
-            fundSubscription.fundSubscription(
-                vrfCoordinatorV2,
-                subscriptionId,
-                link,
-                deployerKey
-            );
+            fundSubscription.fundSubscription(config.vrfCoordinatorV2_5, config.subscriptionId, config.link);
         }
 
-        vm.startBroadcast(deployerKey);
+        vm.startBroadcast();
         Raffle raffle = new Raffle(
-            subscriptionId,
-            gasLane,
-            automationUpdateInterval,
-            raffleEntranceFee,
-            callbackGasLimit,
-            vrfCoordinatorV2
+            config.subscriptionId,
+            config.gasLane,
+            config.automationUpdateInterval,
+            config.raffleEntranceFee,
+            config.callbackGasLimit,
+            config.vrfCoordinatorV2_5
         );
         vm.stopBroadcast();
 
         // We already have a broadcast in here
-        addConsumer.addConsumer(
-            address(raffle),
-            vrfCoordinatorV2,
-            subscriptionId,
-            deployerKey
-        );
+        addConsumer.addConsumer(address(raffle), config.vrfCoordinatorV2_5, config.subscriptionId);
         return (raffle, helperConfig);
     }
 }
