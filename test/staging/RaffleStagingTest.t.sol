b91a3284439250ddcf14e6e8213e8510dc8f6c51
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2024-06-18 15:31:42 -0400

    wip

--- test/staging/RaffleStagingTest.t.sol
+++ test/staging/RaffleStagingTest.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+pragma solidity ^0.8.19;
 
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
